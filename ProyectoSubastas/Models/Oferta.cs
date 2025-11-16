9415abac350441aa562a741ae0dab302c772ac11
Mateo Juarez <mateoporo1234@gmail.com>
2025-11-16 13:28:20 -0300

    se agrega model oferta

---
+++ ProyectoSubastas/Models/Oferta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoSubastas.Models
+{
+    public class Oferta
+    {
+        public int IdOferta { get; set; }
+
+        [Required]
+        [Range(1, double.MaxValue)]
+        public decimal Monto { get; set; }
+
+        public DateTime FechaOferta { get; set; }
+
+        public int IdSubasta { get; set; }
+        public Subasta Subasta { get; set; }
+
+        public int IdPostor { get; set; }
+        public Postor Postor { get; set; }
+
+        public Oferta() { }
+
+        public IList<ValidationResult> Validate()
+        {
+            var ctx = new ValidationContext(this);
+            var results = new List<ValidationResult>();
+            Validator.TryValidateObject(this, ctx, results, true);
+            return results;
+        }
+    }
+}
